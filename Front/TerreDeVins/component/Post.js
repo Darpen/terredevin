7c8921dfb668e727c0c788bd5cf865db0316822b
Maxime <bigot.max@gmail.com>
2020-03-09 17:06:14 +0100

    Avancement page accueil

--- Front/TerreDeVins/component/Post.js
+++ Front/TerreDeVins/component/Post.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 
 class Post extends React.Component{
     render(){
         return(
             <View key={this.props.post.id} style={styles.container}>
-                <Text>{this.props.post.id}</Text>
-                <Text>{this.props.post.title}</Text>
-                <Text>{this.props.post.creator}</Text>
+                <Image 
+                    style={styles.image} 
+                    source={{uri: 'https://www.vinotrip.com/fr/blog/wp-content/uploads/2016/02/exportation-vin1.png'}}
+                />
+                <View style={styles.textes}>
+                    <Text style={styles.titre}>{this.props.post.title}</Text>
+                    <Text style={styles.categorie}>{this.props.post.categories[1].name}</Text>
+                </View>
             </View>
         );
     }
@@ -17,12 +22,31 @@ export default Post;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      borderColor: '#000',
-      borderStyle: 'solid',
-      borderWidth: 0.5,
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        borderColor: '#000',
+        borderStyle: 'solid',
+        borderWidth: 0.5,
     },
+    textes: {
+        marginLeft: 12,
+    },
+    titre:  {
+        fontSize: 20,
+        width: '45%',
+    },
+    categorie: {
+        fontFamily: 'Roboto',
+        fontSize: 12,
+        color: '#7B7B7B',
+        borderLeftColor: '#79C744',
+        borderLeftWidth: 2,
+        paddingLeft: 6,
+    },
+    image: {
+        width: 100,
+        height: 100,
+    }
   });
