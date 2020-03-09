10044bfde6c20e75d20dec03ef7ec7fa2f294785
Maxime <bigot.max@gmail.com>
2020-03-09 15:46:39 +0100

    Mapping du flux rss et création des principaux composants

--- Back/config/bundles.php
+++ Back/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
---
+++ Front/TerreDeVins/component/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class Header extends React.Component{
+
+    render(){
+        return(
+            <View>
+                <Text>TERRE DE VINS</Text>
+            </View>
+        );
+    }
+
+
+}
+
+export default Header;
